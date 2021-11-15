402ef2527465e60e203df58a943374952bdc7dd0
Ricardo Ramirez <rramirez@lcg.unam.mx>
2021-11-15 11:21:07 +0100

    qc spatial final

--- analysis/2_snuc_integration_harmony/plot_qc_spatial_pb.R
+++ analysis/2_snuc_integration_harmony/plot_qc_spatial_pb.R
@@ -16,6 +16,24 @@ library(cowplot)
 path <- "./visium_data/"
 sample_names <- list.files(path)
 
+# Patient info
+patient_info <- readRDS("./markers/visium_patient_anns_revisions.rds")
+
+# Raw qc stats
+qc_feats <- c("sample_names",
+              "Number of Spots Under Tissue")
+
+slide_files <- paste0(path,
+                      sample_names,
+                      "/outs/metrics_summary.csv")
+
+qc_stats <- tibble(sample_names,
+                   qc_stats = map(slide_files, read_csv)) %>%
+  unnest() %>%
+  dplyr::select(all_of(qc_feats))
+
+colnames(qc_stats) <- c("sample_names", "n_spots_under_tissue")
+
 # Spot quantification
 nuclei_quant_path <- "./results/Spotcounts/tissue_spot_counts_"
 
@@ -31,65 +49,65 @@ nuclei_data <- tibble(sample_name = sample_names,
 # Meta data from pseudobulk profiles
 meta_data <- atlas_meta <- readRDS("./processed_visium/integration/ps_integrated_slides.rds")[[1]][["annotations"]] %>%
   rownames_to_column("spot_id") %>%
+  dplyr::select(-patient) %>%
   dplyr::mutate(spot_id = strsplit(spot_id, "_") %>%
                   map_chr(., ~.x[[1]])) %>%
   dplyr::mutate(spot_id = paste0(orig.ident, "..", spot_id)) %>%
-  left_join(nuclei_data, by = "spot_id")
+  left_join(nuclei_data, by = "spot_id") %>%
+  left_join(qc_stats, by = c("orig.ident" = "sample_names")) %>%
+  left_join(patient_info, by = c("orig.ident" = "sample_id"))
+
+# 
 
 # Plot panels
 
 ncells_p <- meta_data %>%
-  group_by(orig.ident) %>%
-  summarise(ncells = length(orig.ident)) %>%
-  ggplot(aes(x = orig.ident, y = ncells)) +
+  group_by(patient_id) %>%
+  summarise(ncells = (length(patient_id))) %>%
+  ggplot(aes(x = patient_id, y = ncells)) +
   geom_bar(stat = "identity") +
   theme_classic() +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_text(size = 11),
-        axis.title.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "cm")) +
+        axis.title.x = element_blank()) +
   ylab("Number of  \n spots")
 
 nCount_RNA_p <- meta_data  %>%
-  ggplot(aes(x = orig.ident, y = nCount_Spatial)) +
+  ggplot(aes(x = patient_id, y = log10(nCount_Spatial))) +
   geom_violin() +
   theme_classic() +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_text(size = 11),
-        axis.title.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "cm")) +
-  ylab("Number of  \n UMIs")
+        axis.title.x = element_blank()) +
+  ylab("Number of  \n UMIs (log10)")
 
 nFeature_RNA_p <-  meta_data  %>%
-  ggplot(aes(x = orig.ident, y = nFeature_Spatial)) +
+  ggplot(aes(x = patient_id, y = log10(nFeature_Spatial))) +
   geom_violin() +
   theme_classic() +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_text(size = 11),
-        axis.title.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "cm")) +
-  ylab("Number of  \n genes")
+        axis.title.x = element_blank()) +
+  ylab("Number of  \n genes (log10)")
 
 percent_mt_p <- meta_data  %>%
-  ggplot(aes(x = orig.ident, y = percent.mt)) +
+  ggplot(aes(x = patient_id, y = percent.mt)) +
   geom_violin() +
   theme_classic() +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_text(size = 11),
-        axis.title.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "cm")) +
+        axis.title.x = element_blank()) +
   ylab("Mitochondrial \n gene percentage")
 
 nuclei_quant_p <- meta_data  %>%
-  ggplot(aes(x = orig.ident, y = count)) +
+  ggplot(aes(x = patient_id, y = count)) +
   geom_violin() +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 90,
                                    hjust = 1,
                                    vjust = 0.5),
         axis.text.y = element_text(size = 11),
-        axis.title.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "cm")) +
+        axis.title.x = element_blank()) +
   ylab("Quantified nuclei \n per spot")
 
 # Generate panel
@@ -98,20 +116,33 @@ panel_p <- cowplot::plot_grid(ncells_p, nCount_RNA_p,
                               nuclei_quant_p,
                               nrow = 5, ncol = 1, align = "hv")
 
-pdf("./processed_visium/initial_qc/qc_distributions.pdf", height = 12, width = 6)
+pdf("./processed_visium/initial_qc/qc_distributions_spatial.pdf", height = 12, width = 6)
 plot(panel_p)
 dev.off()
 
-
+# This is the table that we can use to correlate the loss of cells in snRNAseq
 
 meta_data %>% 
-  group_by(orig.ident) %>%
-  summarise(nspots = length(orig.ident),
-            median_counts_spot = median(nCount_Spatial_filt),
-            median_genes_spot = median(nFeature_Spatial_filt)) %>%
-  print(n = 30) %>%
-  write.table(row.names = F, col.names = T, quote = F, sep = ",",
-            file = "./processed_visium/initial_qc/all_qcs_after_filtering.csv")
+  group_by(patient_id) %>%
+  mutate(nspots = length(patient_id)) %>%
+  dplyr::select(nspots, nCount_Spatial, nFeature_Spatial, count, percent.mt) %>%
+  write_csv(.,
+            file = "./processed_visium/initial_qc/qc_distributions_spatial.csv")
+
+# This is the table that we can use to correlate the loss of cells in snRNAseq
+
+# meta_data %>% 
+#   group_by(patient_id) %>%
+#   mutate(nspots = length(patient_id),
+#          median_counts_spot = median(nCount_Spatial_filt),
+#           median_genes_spot = median(nFeature_Spatial_filt)) %>%
+#   dplyr::select(nspots, median_counts_spot, median_genes_spot, n_spots_under_tissue) %>%
+#   unique() %>%
+#   arrange(patient_id) %>%
+#   mutate(n_spots_under_tissue = sum(n_spots_under_tissue)) %>%
+#   unique() %>%
+#   write.table(row.names = F, col.names = T, quote = F, sep = ",",
+#             file = "./processed_visium/initial_qc/all_qcs_after_filtering.csv")
   
 
 
