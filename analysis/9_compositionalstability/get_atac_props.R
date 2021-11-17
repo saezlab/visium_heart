c50ea63e40b8e4ab39b38b5892de80c7e00625a3
Ricardo Ramirez <rramirez@lcg.unam.mx>
2021-11-17 09:59:09 +0100

    added cell-type recovery from ATAC

--- analysis/9_compositionalstability/get_atac_props.R
+++ analysis/9_compositionalstability/get_atac_props.R
@@ -23,4 +23,25 @@ atac_props <- atac_meta %>%
 
 write.table(atac_props, file = "./results/compositions/atac_compositions.txt", col.names = T, row.names = F, quote = F, sep = "\t")
 
+atac_props$keep <- ifelse((atac_props$atac_n_cells > 10), 
+                         TRUE, FALSE)
 
+cols <- c("TRUE" = "grey",
+          "FALSE" = "black")
+
+# First check the number of cells per patient
+patient_summary_plt_flx <- ggplot(atac_props, 
+                                  aes(y = patient_id, x = cell_type, fill = keep)) +
+  geom_tile() +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        panel.background= element_rect(fill="black", colour="black")) +
+  scale_fill_manual(values = cols,
+                    na.value = 'black') +
+  coord_equal()
+
+pdf("./results/cell_markers/cell_type_recovery_atac.pdf", height = 5, width = 4)
+
+plot(patient_summary_plt_flx)
+
+dev.off()
